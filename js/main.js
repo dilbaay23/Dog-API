b9663615e72ee7d3fe44cd69853da72fadbf91ef
Moon <dilbaay@gmail.com>
2021-03-14 17:34:23 +0100

    Dog Api finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+let timer;
+let deleteFirstPhotoDelay;
+
 async function start() {
-  let response = await fetch("https://dog.ceo/api/breeds/list/all");
-  let data = await response.json();
-  createBreedList(data.message);
+  try {
+    let response = await fetch("https://dog.ceo/api/breeds/list/all");
+    let data = await response.json();
+    createBreedList(data.message);
+  } catch (error) {
+    console.log("There was a problem fetching the breed list.");
+  }
 }
 
 start();
@@ -21,12 +28,48 @@ async function loadByBreed(breed) {
   if (breed != "Choose a dog breed") {
     const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
     const data = await response.json();
-    console.log(data);
-    // createSlideShow(data.message);
+
+    createSlideShow(data.message);
   }
 }
 function createSlideShow(images) {
-  /*   document.getElementById(
-    "slideshow"
-  ).innerHTML = `<div class = "slide" style = "background-image : url('${images[0]}')"></div>`; */
+  let currentPosition = 0;
+  clearInterval(timer);
+  clearTimeout(deleteFirstPhotoDelay);
+
+  if (images.length > 1) {
+    document.getElementById("slideshow").innerHTML = `
+  <div class="slide" style="background-image: url('${images[0]}')"></div>
+  <div class="slide" style="background-image: url('${images[1]}')"></div>
+  
+  `;
+    currentPosition += 2;
+    if (images.length == 2) currentPosition = 0;
+    timer = setInterval(nextSlide, 3000);
+  } else {
+    document.getElementById("slideshow").innerHTML = `
+  <div class="slide" style="background-image: url('${images[0]}')"></div>
+  <div class="slide"></div>
+  
+  `;
+    currentPosition += 2;
+    timer = setInterval(nextSlide, 3000);
+  }
+
+  function nextSlide() {
+    document
+      .getElementById("slideshow")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`
+      );
+    deleteFirstPhotoDelay = setTimeout(function () {
+      document.querySelector(".slide").remove();
+    }, 1000);
+    if (currentPosition + 1 >= images.length) {
+      currentPosition = 0;
+    } else {
+      currentPosition++;
+    }
+  }
 }
